2de8b1f2bcb71041af6cfda09f96da91670efe62
Diana Anton <diana.anton00@e-uvt.ro>
2022-05-29 19:20:43 +0300

    SHA256 | Small changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "./caesar/caesar.cpp"
 #include "./rsa/rsa.cpp"
+#include "./sha256/sha256.cpp"
 
 using namespace std;
 //----------------------------------------------------------------------------//
@@ -9,32 +10,32 @@ int main()
     int option;
     while (true)
     {
-        cout << "1. Caesar cypher" << endl;
-        cout << "2. RSA" << endl;
-        cout << "3. TODO" << endl;
+        cout << "1. RSA" << endl;
+        cout << "2. Caesar cypher" << endl;
+        cout << "3. SHA256" << endl;
         cout << "4. Exit" << endl;
         cout << "Choose an algorithm: "; cin >> option;
         switch(option)
         {
             case 1:
             {
+                cout << "\n------------------------Running RSA cypher---------------------------\n\n";
+                run_rsa();
+                cout << "\n------------------------Done RSA cypher------------------------------\n\n";
+                break;   
+            }
+            case 2:
+            {
                 cout << "\n------------------------Running Caesar cypher------------------------\n\n";
                 run_caesar();
                 cout << "\n------------------------Done Caesar cypher---------------------------\n\n";
                 break;
             }
-            case 2:
-            {
-                cout << "\n------------------------Running RSA cypher---------------------------\n\n";
-                run_rsa();
-                cout << "\n------------------------Done RSA cypher------------------------------\n\n";
-                break;
-            }
             case 3:
             {
-                cout << "\n------------------------Running TODO cypher------------------------\n\n";
-
-                cout << "\n------------------------Running TODO cypher------------------------\n\n";
+                cout << "\n------------------------Running SHA256 cypher------------------------\n\n";
+                run_sha256();
+                cout << "\n------------------------Done SHA256 cypher---------------------------\n\n";
                 break;
             }
             case 4:
